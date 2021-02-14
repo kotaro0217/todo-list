a387754aca506e75c22fbe9ffa8ed6c105c3a785
kotaro0217 <kotaro96480321@gamil.com>
2021-02-14 21:07:38 +0900

    アクション定義の実装

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,34 @@
+class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    Task.create(text: params[:text])
+    redirect_to root_path
+  end
+
+  def destroy
+    task = Task.find(params[:id])
+    task.destroy
+    redirect_to root_path
+  end
+
+  def edit
+    task = Task.find(params[:id])
+  end
+
+  def update
+    task = Task.find(params[:id])
+    if task.update(text: params[:text])
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+
+end
